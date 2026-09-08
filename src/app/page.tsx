3fb6fa4369f9854245c8d56ca3290b9d1a76b2e7
expresslimited25 <expresslimited25@gmail.com>
2026-09-08 15:24:19 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import WandrApp from "@/components/WandrApp";
+"use client";
+import dynamic from "next/dynamic";
+
+const WandrApp = dynamic(() => import("@/components/WandrApp"), { ssr: false });
 
 export default function Page() {
   return <WandrApp />;
