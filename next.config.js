f5e183d625280b5b23318c4fbba75440ab8f3627
expresslimited25 <expresslimited25@gmail.com>
2026-09-26 14:16:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/index-landing.html",
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
